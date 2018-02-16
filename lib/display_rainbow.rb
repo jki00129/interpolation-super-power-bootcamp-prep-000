d9ca256e17a753b2801b84625f3404b6e3712038
Jeewon Kim <jki00129@gmail.com>
2018-02-16 00:04:01 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
  colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
- letters = ["R","O","Y"]
-puts "The first letter in the alphabet is: #{letters[1]}"
+ letters = ["R","O","Y","G","B","I","V"]
+puts "The first color is  #{letters[0]}"
 puts "The second letter in the alphabet is: #{letters[0]}"
 puts "The third letter in the alphabet is: #{letters[2]}"
 def display_rainbow(colors)
